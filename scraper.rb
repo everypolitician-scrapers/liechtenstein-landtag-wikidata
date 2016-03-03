f4cc967881575e0ef2f2ae229f499279fef73fc5
Tony Bowden <tony@tmtm.com>
2016-03-03 22:10:50 +0000

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,12 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'wikidata/fetcher'
+
+names = EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://de.wikipedia.org/wiki/Liste_der_Mitglieder_des_liechtensteinischen_Landtags_(2013)',
+  after: '//span[@id="Liste_der_Mitglieder"]',
+  xpath: '//table[.//th[contains(.,"Fraktion")]]//tr[td]//td[1]//a[not(@class="new")]/@title',
+) 
+EveryPolitician::Wikidata.scrape_wikidata(names: { de: names })
+
