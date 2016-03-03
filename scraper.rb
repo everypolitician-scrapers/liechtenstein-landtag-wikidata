1d9887247fe69206f60d88a0548fea564543f26b
Tony Bowden <tony@tmtm.com>
2016-03-03 22:22:15 +0000

    Add previous term

--- scraper.rb
+++ scraper.rb
@@ -7,6 +7,11 @@ names = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://de.wikipedia.org/wiki/Liste_der_Mitglieder_des_liechtensteinischen_Landtags_(2013)',
   after: '//span[@id="Liste_der_Mitglieder"]',
   xpath: '//table[.//th[contains(.,"Fraktion")]]//tr[td]//td[1]//a[not(@class="new")]/@title',
-) 
+)  | 
+EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://de.wikipedia.org/wiki/Liste_der_Mitglieder_des_liechtensteinischen_Landtags_(2009)',
+  after: '//span[@id="Liste_der_Mitglieder"]',
+  xpath: '//table[.//th[contains(.,"Fraktion")]]//tr[td]//td[1]//a[not(@class="new")]/@title',
+)  
 EveryPolitician::Wikidata.scrape_wikidata(names: { de: names })
 
