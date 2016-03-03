1f107095a542ffac4183c9d17afcfb18006e0541
Tony Bowden <tony@tmtm.com>
2016-03-03 22:29:27 +0000

    Add 2005

--- scraper.rb
+++ scraper.rb
@@ -12,6 +12,11 @@ EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://de.wikipedia.org/wiki/Liste_der_Mitglieder_des_liechtensteinischen_Landtags_(2009)',
   after: '//span[@id="Liste_der_Mitglieder"]',
   xpath: '//table[.//th[contains(.,"Fraktion")]]//tr[td]//td[1]//a[not(@class="new")]/@title',
+)  | 
+EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://de.wikipedia.org/wiki/Liste_der_Mitglieder_des_liechtensteinischen_Landtags_(2005)',
+  after: '//span[@id="Liste_der_Mitglieder"]',
+  xpath: '//table[.//th[contains(.,"Fraktion")]]//tr[td]//td[1]//a[not(@class="new")]/@title',
 )  
 EveryPolitician::Wikidata.scrape_wikidata(names: { de: names })
 
